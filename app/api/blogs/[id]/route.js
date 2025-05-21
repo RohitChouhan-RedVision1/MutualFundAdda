1a06cb9f314e574285f12088165c96620c2d0f61
RohitChouhan-RedVision1 <Rohit.Chouhan@redvisionglobal.com>
2025-05-21 13:08:12 +0530

    changes done

--- app/api/blogs/[id]/route.js
+++ app/api/blogs/[id]/route.js
@@ -2,25 +2,28 @@ import { NextResponse } from 'next/server';
 import BlogsModel from '@/lib/models/BlogModel';
 import { ConnectDB } from '@/lib/db/ConnectDB';
 import cloudinary from '@/lib/cloudinary';
+import path from 'path';
+import fs from 'fs';
 import { slugify } from '@/lib/functions';
-
+ 
+ 
 export async function DELETE(req, { params }) {
-    const { id } = await params;
-
+    const { id } = params;
+ 
     try {
         await ConnectDB();
-
+ 
         // Find the blog by ID
         const blog = await BlogsModel.findById(id);
-
+ 
         if (!blog) {
             return NextResponse.json({ error: 'Blog not found' }, { status: 404 });
         }
-
+ 
         const publicId = blog.image.public_id;
         if (publicId) {
             const result = await cloudinary.uploader.destroy(publicId);
-
+ 
             if (result.result !== 'ok') {
                 return NextResponse.json({ error: 'Failed to delete image from Cloudinary' }, { status: 500 });
             }
@@ -32,64 +35,74 @@ export async function DELETE(req, { params }) {
         return NextResponse.json({ error: 'Failed to delete blog' }, { status: 500 });
     }
 }
-
+ 
 // GET Blog by ID
 export async function GET(req, { params }) {
-    const { id } = await params; // Extract ID from params
-
+    const { id } = params; // Extract ID from params
+ 
     try {
         await ConnectDB(); // Ensure DB connection
         const blog = await BlogsModel.findById(id); // Properly await the findById function
-
+ 
         if (!blog) {
             return NextResponse.json({ error: 'Blog not found' }, { status: 404 });
         }
-
+ 
         return NextResponse.json({ blog }, { status: 200 });
     } catch (error) {
         console.error('Error fetching blog:', error);
         return NextResponse.json({ error: 'Error while fetching blog' }, { status: 500 });
     }
 }
-
+ 
 export async function PUT(req, { params }) {
-    const { id } = await params; // Extract blog ID from params
+    const { id } = params; // Extract blog ID from params
     const data = await req.formData(); // Retrieve FormData from request
-
     try {
         await ConnectDB(); // Ensure DB connection
-
+ 
         // Find the blog by ID
         const blog = await BlogsModel.findById(id);
-
+ 
         if (!blog) {
             return NextResponse.json({ error: 'Blog not found' }, { status: 404 });
         }
-
+        const uploadDirectory = path.join(process.cwd(), 'public/images');
+ 
+        // Ensure the upload directory exists
+        if (!fs.existsSync(uploadDirectory)) {
+            fs.mkdirSync(uploadDirectory, { recursive: true });
+        };
         // Check if there's a new image to upload/update
         let newImageUrl = blog.image.url; // Keep the current image URL by default
         let newPublicId = blog.image.public_id; // Keep the current Cloudinary public_id by default
-
+        const image = data.get('image'); // Get the new image from form data
         // Handle image only if it's provided and not 'null'
         if (data.get('image') && data.get('image') !== 'null') {
-            // If there's an old image, delete it from Cloudinary
-                if (blog.image.public_id) {
-                    const result = await cloudinary.uploader.destroy(blog.image.public_id);
-
-                    if (result.result !== 'ok') {
-                        return NextResponse.json({ error: 'Failed to delete old image from Cloudinary' }, { status: 500 });
-                    }    
+            const publicId = blog.image?.public_id;
+            if (publicId) {
+                const result = await cloudinary.uploader.destroy(publicId);
+                if (result.result !== 'ok') {
+                    return NextResponse.json({ error: 'Failed to delete old image from Cloudinary' }, { status: 500 });
                 }
-
+            }
+            // Generate a unique filename
+            const a = await image.arrayBuffer();
+            const buffer = Buffer.from(a)
+            const uniqueFilename = `${Date.now()}_${image.name}`;
+            const filePath = path.join(uploadDirectory, uniqueFilename);
+            await fs.promises.writeFile(filePath, buffer);
+            // Return the file path as a response
+            const fileUrl = `public/images/${uniqueFilename}`
             // Upload the new image to Cloudinary
-            const uploadResult = await cloudinary?.uploader?.upload(data.get('image'), {
+            const uploadResult = await cloudinary.uploader.upload(fileUrl, {
                 folder: 'mutualfundadda/blogs',
             });
-
+ 
             newImageUrl = uploadResult.secure_url;
             newPublicId = uploadResult.public_id;
         }
-
+ 
         // Update the blog with the new data
         const updatedBlog = await BlogsModel.findByIdAndUpdate(
             id,
@@ -108,7 +121,7 @@ export async function PUT(req, { params }) {
             },
             { new: true } // Return the updated document
         );
-
+ 
         return NextResponse.json({ blog: updatedBlog }, { status: 200 });
     } catch (error) {
         console.error('Error updating blog:', error);
